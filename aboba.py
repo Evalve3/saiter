6ccb40d53656b42c9c7530b9b297f5cbddcb3858
Evalve3 <evalve39@gmail.com>
2022-07-06 00:48:22 +0300

    govno no rabotaet

--- aboba.py
+++ aboba.py
@@ -1,45 +1,87 @@
 import urllib.request
-import requests
-import time
+import psycopg2
+from psycopg2 import Error
+from itertools import permutations
+from string import ascii_lowercase
+import signal
+
+class TimeoutException(Exception):
+    def __init__(self, *args, **kwargs):
+        pass
 
 
-class Timer:
-    def __init__(self, func):
-        a = time.time()
-        func()
-        self.tm = time.time() - a
+def signal_handler(signum, frame):
+    raise TimeoutException()
 
 
-def check1():
+def time_break(func):
+    """
+    Декоратор, останавливающий работу декорируемой функции, если её
+    выполнение, заняло более 10 секунд
+    """
+
+    def wrapper(*args, **kwargs):
+        try:
+            # print("Запускаем тестируемую функцию")
+            signal.signal(signal.SIGALRM, signal_handler)
+            signal.alarm(5)
+            res = func(*args, **kwargs)
+            signal.alarm(0)
+            # print("Нормальное завершение")
+            return res
+        except TimeoutException:
+            pass
+            # print("Time out")
+        except:
+            pass
+            # print("Some other exception")
+
+    return wrapper
+
+
+@time_break
+def check_site(url):
     try:
-        requests.head("https://asdf-vm.com")
-        print("Sucesfull")
-    except BaseException as govno:
-        print(govno)
+        urllib.request.urlopen(url)
+        return url
+    except:
+        return None
 
 
-def check2():
-    try:
-        urllib.request.urlopen("https://asdf-vm.com")
-        print("Sucesfull")
-    except BaseException as govno:
-        print(govno)
-
-
-def who_winner(c1=0, c2=0, c1_sum=0, c2_sum=0):
-    for i in range(20000):
-        a = Timer(check1).tm
-        c1_sum += a
-        b = Timer(check2).tm
-        c2_sum += b
-        if a < b:
-            c1 += 1
-        else:
-            c2 += 1
-
-    print("check 1 time wins =", c1, "\ncheck 2 time wins", c2)
-    print("check 1 sum tine =", c1_sum, "\ncheck 2 sum time=", c2_sum)
+def add_site_com(cursor, connection, url_for_check):
+    url_for_check = 'https://' + url_for_check + '.com'
+    url = check_site(url_for_check)
+    if url:
+        pgs_query = f'''
+                            INSERT INTO sites_com (url)
+                            VALUES
+                            ('{url}');
+                '''
+        cursor.execute(pgs_query)
+        connection.commit()
+        print("site add")
 
 
 if __name__ == '__main__':
-    who_winner()
+
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      # пароль, который указали при установке PostgreSQL
+                                      password="123",
+                                      host="localhost",
+                                      port="5432",
+                                      database="Sites")
+        cursor = connection.cursor()
+        a = set(permutations(ascii_lowercase, 2))
+        for i in a:
+            tmp = ''
+            for j in i:
+                tmp += j
+            print(tmp)
+            add_site_com(cursor, connection, tmp)
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
